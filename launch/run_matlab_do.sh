a1c52fd5e9e18a8a4640639db7b309ac25409400
Travers Rhodes <traversr@andrew.cmu.edu>
2018-07-25 17:01:13 -0400

    fix up code paths to run on NUC

--- launch/run_matlab_do.sh
+++ launch/run_matlab_do.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pushd ~/code/feedBotCode/src/do_matlab_ros
+pushd ~/feedBotCode/src/do_matlab_ros
 matlab -nodisplay -nodesktop -r "try, run KinectInteraction.m; end; quit"
 popd
