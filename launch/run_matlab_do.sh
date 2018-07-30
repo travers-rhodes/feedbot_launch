fd1bc559d9edf6d21dabcdf15c037fa458104ae3
Travers Rhodes <traversr@andrew.cmu.edu>
2018-07-29 22:57:26 -0400

    checkpoint

--- launch/run_matlab_do.sh
+++ launch/run_matlab_do.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 pushd ~/feedBotCode/src/do_matlab_ros
-matlab -nodisplay -nodesktop -r "try, run KinectInteraction.m; end; quit"
+#matlab -nodisplay -nodesktop -r "try, run KinectInteraction.m; end; quit"
+matlab -nodisplay -nodesktop -r "run KinectInteraction.m"
 popd
